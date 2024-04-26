f6d4020a7d28dbc496d5499c08d38aaf3e323c66
OrhunBegen1 <orhunbegen@outlook.com>
2024-04-26 14:20:24 +0300

    c files txt files created

--- src/Creatiny.h
+++ src/Creatiny.h
@@ -6,8 +6,43 @@
 
 // Function declarations
 
+void OpenFoodListFile();
+void OpenOrdersFile();
+void CreateAccount();
+void Login();
+
 
 // Function definitions
+
+void OpenFoodListFile() {
+    FILE *file = fopen("FoodList.txt", "r");
+    if (file == NULL) {
+        printf("Error: FoodList.txt file could not be opened.\n");
+        exit(1);
+    }
+    fclose(file);
+}
+void OpenOrdersFile() {
+    FILE *file = fopen("Orders.txt", "r");
+    if (file == NULL) {
+        printf("Error: Orders.txt file could not be opened.\n");
+        exit(1);
+    }
+    fclose(file);
+}
+
+void CreateAccount() {
+    /* 
+        
+
+     */
+}
+void Login() {
+    /* 
+        
+
+     */
+}
 
 
 #endif // CREATINY_H
