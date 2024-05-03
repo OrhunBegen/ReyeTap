5f3f4dd96bbe537fcbfbcdd6c6c7faae3b2860e2
rumeysa222 <167959089+rumeysa222@users.noreply.github.com>
2024-05-03 20:26:25 +0300

    files Create

---
+++ src/Customer.c
@@ -0,0 +1,11 @@
+#include "Customer.h"
+
+// Implement your functions here
+
+// Example function
+void greetCustomer() {
+    printf("Welcome, customer!\n");
+    printMessage("Hello, customer!");
+}
+
+// Implement other functions here
---
+++ src/Customer.h
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+void printMessage(const char* message) {
+    printf("%s\n", message);
+}
+
+#endif // CUSTOMER_H
