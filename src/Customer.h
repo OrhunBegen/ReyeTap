32cface5c090bfed128c61e7f835cd7fa4cf429c
elifyarensakar <146104421+elifyarensakar@users.noreply.github.com>
2024-05-06 16:19:58 +0300

    customer.h

--- src/Customer.h
+++ src/Customer.h
@@ -56,9 +56,9 @@ void addCustomer()
     printf("Musteri telefonu : %s \n", c1.phone);
     printf("Musteri email : %s \n", c1.email);
     FILE *file = fopen("musteri.dat","a+b");
-    fwrite(&c1, sizeof(Customer), 1, ptr);
-    fclose(ptr);
-    printf("\n\n%d numarali musteri eklendi \n", c1.id);
+    fwrite(&c1, sizeof(Customer), 1, Ptr);
+    fclose(Ptr);
+    printf("\n\n%d numarali musteri eklendi \n", numara);
 
 }
 void CustomerList()
@@ -73,45 +73,8 @@ void deleteCustomer()
 {
 
 }
-void MenuForCustomer()
-    void CustomerMenu() {
-        int secim;
-        printf("\n\t MUSTERI ISLEMLERI \n\n");
-        printf("\t1-MUSTERI EKLE \n");
-        printf("\t2-MUSTERI LISTELE \n");
-        printf("\t3-MUSTERI GUNCELLE \n");
-        printf("\t4-MUSTERI SIL \n");
-        printf("\t0-ANA MENU \n");
-        printf("\tSECIMINIZ : ");
-        scanf("%d", &secim);
-        system("cls");
-        while (secim != 0) {
-            switch (secim) {
-                case 1:
-                    addCustomer();
-                    break;
-                case 2:
-                    CustomerList();
-                    break;
-                case 3:
-                    updateCustomer();
-                    break;
-                case 4:
-                    deleteCustomer();
-                    break;
-                case 0:
-                    break;
-                default:
-                    printf("hatali secim yaptiniz \n");
-                    break;
-            }
-            secim = CustomerMenu();
-        }
-    }
 
-    int main() {
-        CustomerMenu();
-        return 0;
-    }
+
+    
 
 #endif // CUSTOMER_H
