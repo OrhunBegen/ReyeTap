f7763b28ec06b26597212bc0139a39e3403a1bcc
elifyarensakar <146104421+elifyarensakar@users.noreply.github.com>
2024-05-22 15:45:18 +0300

    kitchen.c

--- src/Kitchen.c
+++ src/Kitchen.c
@@ -1,7 +1,19 @@
 #include "Kitchen.h"
+#include "Restaurant.h"
+#include "Customer.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 int main() {
-    // Your code here
-    printf("Hello, World!\n");
+    Yemek yemekListesi[MAX_YEMEK];
+    Siparis siparisListesi[MAX_SIPARIS];
+
+    int yemekSayisi = yemekleriOku(yemekListesi, "FoodList.txt");
+    int siparisSayisi = siparisleriOku(siparisListesi, "OrderList.txt");
+
+    siparisleriYonet(yemekListesi, yemekSayisi, siparisListesi, siparisSayisi);
+
     return 0;
 }
--- src/Kitchen.h
+++ src/Kitchen.h
@@ -9,14 +9,82 @@
 #include <stdbool.h>
 
 
-//struct for the cooks in the kitchen
-typedef struct Cook
-{
-    int cookID;
-    char cookerTime[50];
-    
-} Cook;
 
+#define MAX_YEMEK 100
+#define MAX_SIPARIS 100
+#define MAX_ASCILAR 3
+
+/*typedef struct {
+    char yemekAdi[50];
+    int yemekFiyati;
+    int hazirlanmaSuresi;
+    int mevcut;
+} Yemek;
+
+typedef struct {
+    int siparisID;
+    char yemekAdi[50];
+    int siparisZamani;
+    int hazirlanmaZamani;
+} Siparis;
+
+typedef struct {
+    int bitisZamani;
+} Asci;
+
+
+// Aşçı kuyruk yönetimi
+void siparisleriYonet(Yemek yemekListesi[], int yemekSayisi, Siparis siparisListesi[], int siparisSayisi) {
+    Asci ascilar[MAX_ASCILAR] = {{0}, {0}, {0}};
+
+    for (int i = 0; i < siparisSayisi; i++) {
+        int minBitisZamani = ascilar[0].bitisZamani;
+        int secilenAsci = 0;
+
+        for (int j = 1; j < MAX_ASCILAR; j++) {
+            if (ascilar[j].bitisZamani < minBitisZamani) {
+                minBitisZamani = ascilar[j].bitisZamani;
+                secilenAsci = j;
+            }
+        }
+
+        for (int k = 0; k < yemekSayisi; k++) {
+            if (strcmp(yemekListesi[k].yemekAdi, siparisListesi[i].yemekAdi) == 0 && yemekListesi[k].mevcut) {
+                int baslamaZamani = (minBitisZamani > siparisListesi[i].siparisZamani) ? minBitisZamani : siparisListesi[i].siparisZamani;
+                siparisListesi[i].hazirlanmaZamani = baslamaZamani + yemekListesi[k].hazirlanmaSuresi;
+                ascilar[secilenAsci].bitisZamani = siparisListesi[i].hazirlanmaZamani;
+                printf("Sipariş ID: %d, Yemek: %s, Başlama Zamani: %d, Bitiş Zamani: %d\n", siparisListesi[i].siparisID,
+                       siparisListesi[i].yemekAdi, baslamaZamani, siparisListesi[i].hazirlanmaZamani);
+                break;
+            }
+        }
+    }
+}
+*/
+
+// aşcı sayısını belirle 
+
+typedef struct {
+    int asciID;
+
+} Asci;
+
+int asciSayisiBelirle()
+{
+    int asciSayisi;
+    printf("Asci sayisini giriniz: ");
+    scanf("%d", &asciSayisi);
+    return asciSayisi;
+}
+
+// girilen aşçı sayısı kadar aşçı oluştur
+void asciOlustur(Asci ascilar[], int asciSayisi)
+{
+    for (int i = 0; i < asciSayisi; i++)
+    {
+        ascilar[i].asciID = i;
+    }
+}
 
 
 #endif // KITCHEN_H
